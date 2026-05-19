2c2ec17c3eaadbf52832d7dd1647f7193b8dcebe
Artli22 <ludinlima2514@gmail.com>
2026-05-19 13:24:53 -0600

    5/12 test aprobados

--- password-meter/src/utils/passwordStrength.ts
+++ password-meter/src/utils/passwordStrength.ts
@@ -1,11 +1,19 @@
 export type PasswordStrength =
   | "vacía"
   | "débil"
-  | "media";
+  | "media"
+  | "fuerte"
+  | "muy fuerte";
 
 export function getPasswordStrength(password: string): PasswordStrength {
   if (password.length === 0) return "vacía";
   if (password.length < 8) return "débil";
+
+    const hasNumber = /\d/.test(password);  
+    const hasSymbol = /[!@#$%^&*(),.?":{}|<>]/.test(password);
+
+    if (hasNumber && hasSymbol) return "muy fuerte";
+    if (hasNumber) return "fuerte";
 
   return "media";
 }
