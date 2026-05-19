01b02f2ac0001004ddd5984d478d1a3fb0a8fbb3
Artli22 <ludinlima2514@gmail.com>
2026-05-19 13:19:39 -0600

    Recuperar App.tsx + 3/12 test aprobados

---
+++ password-meter/src/App.tsx
@@ -0,0 +1,9 @@
+import PasswordStrengthMeter from "./components/PasswordStrengthMeter";
+
+export default function App() {
+  return (
+    <main>
+      <PasswordStrengthMeter />
+    </main>
+  );
+}
--- password-meter/src/utils/passwordStrength.ts
+++ password-meter/src/utils/passwordStrength.ts
@@ -1,3 +1,11 @@
-export function getPasswordStrength(password: string) {
-  return "";
+export type PasswordStrength =
+  | "vacía"
+  | "débil"
+  | "media";
+
+export function getPasswordStrength(password: string): PasswordStrength {
+  if (password.length === 0) return "vacía";
+  if (password.length < 8) return "débil";
+
+  return "media";
 }
